f58b4227d370eda5a5f94fd1e943bfa45762d492
rafrex <code@rafrex.com>
2017-05-29 12:37:30 -0700

    Make renderers more concise

--- src/basicRenderer.js
+++ src/basicRenderer.js
@@ -1,50 +1,37 @@
 // @flow
 import React from 'react';
 
-const Heading = (props: { level: number, children: any, key: number }) => {
-  const H = `h${props.level}`;
-  return <H key={props.key}>{props.children}</H>;
-};
-
-const Paragraph = (props: { children: any, key: number }) => {
-  return <p key={props.key}>{props.children}</p>;
-};
-
-const Link = (props: {
-  href: string,
-  title?: string,
-  children: any,
-  key: number,
-}) => {
-  return (
-    <a href={props.href} title={props.title} key={props.key}>
-      {props.children}
-    </a>
-  );
-};
-
-const Emph = (props: { children: any, key: number }) => {
-  return <em key={props.key}>{props.children}</em>;
-};
-
-const Strong = (props: { children: any, key: number }) => {
-  return <strong key={props.key}>{props.children}</strong>;
-};
-
-const InlineBreak = (props: { key: number }) => {
-  return <br key={props.key} />;
-};
-
-const BlockBreak = (props: { key: number }) => {
-  return <br key={props.key} />;
-};
-
 export default {
-  Heading,
-  Paragraph,
-  Link,
-  Emph,
-  Strong,
-  InlineBreak,
-  BlockBreak,
+  Heading(props: { level: number, children: any, key: number }) {
+    const H = `h${props.level}`;
+    return <H key={props.key}>{props.children}</H>;
+  },
+
+  Paragraph(props: { children: any, key: number }) {
+    return <p key={props.key}>{props.children}</p>;
+  },
+
+  Link(props: { href: string, title?: string, children: any, key: number }) {
+    return (
+      <a href={props.href} title={props.title} key={props.key}>
+        {props.children}
+      </a>
+    );
+  },
+
+  Emph(props: { children: any, key: number }) {
+    return <em key={props.key}>{props.children}</em>;
+  },
+
+  Strong(props: { children: any, key: number }) {
+    return <strong key={props.key}>{props.children}</strong>;
+  },
+
+  InlineBreak(props: { key: number }) {
+    return <br key={props.key} />;
+  },
+
+  BlockBreak(props: { key: number }) {
+    return <br key={props.key} />;
+  },
 };
